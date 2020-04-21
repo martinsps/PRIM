274dd4324c0a0933176f2e288b5569790718ddc9
Martín <martinsps96@gmail.com>
2020-04-21 13:12:30 +0200

    Change current rule list to an algorithm variable

--- src/errors.py
+++ src/errors.py
@@ -2,9 +2,10 @@ class Error(Exception):
     """Base class for exceptions in this module."""
     pass
 
+
 class UserInputError(Error):
-    """ Error que indica que el usuario ha cometido un error
-    indicando algún parámetro o en la entrada. """
+    """ Error that indicates that the user has made a
+     mistake with any input data."""
 
     def __init__(self, message):
         self.message = message
